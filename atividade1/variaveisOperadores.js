e75815b35dab8a8f9774b3db9cf0623c2c6fb320
André Luis <113619337+AndreLuisALCPS@users.noreply.github.com>
2024-03-22 10:35:15 -0300

    Update variaveisOperadores.js

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -4,13 +4,13 @@ const Num_D = "6";
 const Num_E = "7";
 
 let c = Num_A + Num_B;
-console.log('O resultado da soma entre ' + Num_A + ' e ' + Num_B + ' é ' + c);
+console.log('Resultado da soma entre ' + Num_A + ' e ' + Num_B + ' = ' + c);
 
 c = Num_A - Num_B; 
-console.log(`O resultado da subtração entre ${Num_A} e ${Num_B} é ${c}`);
+console.log(`Resultado da subtração entre ${Num_A} e ${Num_B} = ${c}`);
 
 c = Num_A * Num_B;
-console.log(`O resultado da multiplicação entre ${Num_A} e ${Num_B} é ${c}`);
+console.log(`Resultado da multiplicação entre ${Num_A} e ${Num_B} = ${c}`);
 
 c = Num_A / Num_B;
-console.log(`O resultado da divisão entre ${Num_A} e ${Num_B} é ${c}`);
+console.log(`Resultado da divisão entre ${Num_A} e ${Num_B} = ${c}`);
